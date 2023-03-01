bbf341b82d3f35fbf8caf06f08ff884f2480dc70
ArditVezaj <arditvezaj1@gmail.com>
2023-03-01 22:50:23 +0100

    input listening events and style

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+const [outputItem, setOutputItem] = useState("lemon");
+
+const changeText = (event) => {
+  setOutputItem(event.target.value)
+}
+
+const submitInput = (event) => {
+  event.preventDefault();
+  
+};
+
   return (
     <div className="App">
         <img src={logo} className="App-logo" alt="logo" />
         <div>
           <h2>My Shopping List</h2>
-          <ul on>
-            <li>Lemons</li>
+          <ul>
+            <li>{outputItem}</li>
             <li>Rice</li>
             <li>Bread</li>
           </ul>
+          <form onSubmit={submitInput}>
+          <input type="text" onChange={changeText}/>
+          </form>
         </div>
     </div>
   );
