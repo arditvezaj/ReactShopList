66befea0d75dcb2278b639984e8fb9b8ca1a09e2
ArditVezaj <arditvezaj1@gmail.com>
2023-03-05 21:13:48 +0100

    added full logic

--- src/App.js
+++ src/App.js
@@ -1,33 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-const [outputItem, setOutputItem] = useState("lemon");
+  const [outputItems, setOutputItems] = useState([
+    { name: "Bread", id: "1" },
+    { name: "Soda", id: "2" },
+  ]);
+  const [enteredItem, setEnteredItem] = useState("");
+  const [isValid, setIsValid] = useState(true);
 
-const changeText = (event) => {
-  setOutputItem(event.target.value)
-}
+  const changeText = (event) => {
+    setEnteredItem(event.target.value);
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
+  };
 
-const submitInput = (event) => {
-  event.preventDefault();
-  
-};
+  const submitInput = (event) => {
+    event.preventDefault();
+    if (enteredItem.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
+    setOutputItems((prevItems) => {
+      const updatedItems = [...prevItems];
+      updatedItems.push({ name: enteredItem, id: Math.random().toString() });
+
+      return updatedItems;
+    });
+    setEnteredItem("");
+  };
 
   return (
     <div className="App">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div>
-          <h2>My Shopping List</h2>
-          <ul>
-            <li>{outputItem}</li>
-            <li>Rice</li>
-            <li>Bread</li>
-          </ul>
-          <form onSubmit={submitInput}>
-          <input type="text" onChange={changeText}/>
-          </form>
-        </div>
+      <img src={logo} className="App-logo" alt="logo" />
+      <div>
+        <h2>My Shopping List</h2>
+        <ul>
+          {outputItems.map((item) => (
+            <li key={item.id} id={item.id}>
+              {item.name}
+            </li>
+          ))}
+        </ul>
+        <form onSubmit={submitInput}>
+          <input type="text" value={enteredItem} onChange={changeText} />
+          <button>Submit</button>
+        </form>
+      </div>
     </div>
   );
 }
